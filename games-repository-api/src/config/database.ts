1d748c676e4ef072e1ce91c10e89c8ae17296351
Juan <josemargri3@gmail.com>
2024-12-03 10:27:00 -0500

    fix: Desestructurar elementos de la variable config

--- games-repository-api/src/config/database.ts
+++ games-repository-api/src/config/database.ts
@@ -2,14 +2,16 @@
 import { Sequelize } from 'sequelize-typescript';
 import { config } from './general'; 
 
+const { dbHost, dbPort, dbUsername, dbPassword, dbDatabase } = config;
+
 // Nos conectamos a la base de datos usando las variables de configuración
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: config.dbHost,           
-    port: config.dbPort,           
-    username: config.dbUsername,   
-    password: config.dbPassword,   
-    database: config.dbDatabase,   
+    host: dbHost,           
+    port: dbPort,           
+    username: dbUsername,   
+    password: dbPassword,   
+    database: dbDatabase,   
     models: [__dirname + '/models'],
     logging: false,
 });
