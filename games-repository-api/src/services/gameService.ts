ea266760e024bc4880f2eed03694cbb9fa4001b9
harrisonzm <142773639+Hzuletam@users.noreply.github.com>
2024-12-03 04:56:32 -0500

    cambios tsconfig, terminado adición de atributos a GameService

--- games-repository-api/src/services/gameService.ts
+++ games-repository-api/src/services/gameService.ts
@@ -56,35 +56,37 @@ export default class GameService {
             game.objectives = instance.Objectives;
             game.time = instance.Time;
 
-
             //insert game into games
             games = {
                 ...games,
                 game
             }; 
         }
-        const res: {status: number , games:object, message:string} = {
+            const res: {status: number , data:object} = {
             "status": 0,
-            "games": {},
-            "message": ""
+            "data": {},
+            
         }
 
         res.status = 201;
-        res.games = games;
-    
+        res.data = games;
+
         return res;
     }
 
     getGame(id:number): object{
         let game:Game | null = null;
-        const res:{status: number, game:object, message:string} = {
+        const res:{status: number, data:object} = {
             "status": 0,
-            "game": {},
-            "message" : ""
+            "data": {},
         }
         for(let instance of this._storage) if( instance.Id === id) game = instance ;
         if(game){
-            let instance:{id:number , name:string, date:string, descriptioon:string} = {
+            let instance:{
+                id:number, 
+                name:string, 
+                date:string, 
+                descriptioon:string} = {
 
                 "id" : game.Id,
                 "name":game.Name,
@@ -93,61 +95,82 @@ export default class GameService {
             };
 
             res.status = 201;
-            res.game = game; 
+            res.data = game; 
+            return res;
 
         }else{
-            res.status = 400;
-            res.message = " the id doesn't exist";
+            throw(new Error("id doesn't exist")); 
+        }
+    }
+
+    postGame(data:{name:string, 
+        date:string, 
+        purpose:string, 
+        thematic:object, 
+        genre:string, 
+        materials:object, 
+        objectives:string, 
+        time:string}): object
+    {
+        const res:{status:number, data:object} = 
+        {
+            "status" : 0,
+            "data" : {},
+        }
+        try{
+            //take out the data
+            const game:{id:number,
+                name:string, 
+                date:string, 
+                purpose:string, 
+                thematic:object, 
+                genre:string, 
+                materials:object, 
+                objectives:string, 
+                time:string}= {
+                id:this._storage.length + 1,
+                name:data.name,
+                date:data.date,
+                purpose:data.purpose,
+                thematic: data.thematic,
+                genre:data.genre,
+                materials:data.materials,
+                objectives:data.objectives,
+                time:data.time
+            }
             
+            if(!data.name || !data.purpose || !data.date || !data.thematic || !data.genre || !data.materials || !data.objectives || !data.time) throw(new Error("not enough data"));
+
+            //create new game 
+            const newGame:Game = new Game({
+                id : game.id,
+                name : game.name,
+                date : game.date,
+                purpose : game.purpose,
+                thematic : game.thematic,
+                genre : game.genre,
+                materials : game.materials,
+                objectives : game.objectives,
+                time : game.time
+            })
+
+            //get it into the storage
+            this._storage.push(newGame);
+            res.status = 201;
+            res.data = game;
+        }
+        catch(err){
+            throw(new Error("bad request"));
         }
         return res;
     }
 
-postGame(data:{name:string, description:string, date:string}): object
-{
-    const res:{status:number, game:object} = 
-    {
-        "status" : 0,
-        "game" : {},
-    }
-    try{
-        //take out the data
-        const game:{id:number,name:string, description:string, date:string}= {
-            id:this._storage.length + 1,
-            name:data.name,
-            description:data.description,
-            date:data.date
-        }
-        
-        if(!data.name || !data.description || !data.date) throw(Error("not enough data "));
-
-        //create new game 
-        const newGame:Game = new Game({
-            id: game.id,
-            name:game.name, 
-            date:game.date, 
-            purpose:game.description
-        })
-
-        //get it into the storage
-        this._storage.push(newGame);
-        res.status = 201;
-        res.game = game;
-    }
-    catch(err){
-        throw("bad request")
-    }
-    return res;
-}
-
     editGame( id:number, changes:{name:string, description:string, fecha:string}): object
     {
         let game : Game|null  = null; 
-
         for(let i = 0; i<this._storage.length; i++){
             if(this._storage.at(i)?.Id == id) game = this._storage[i];
         }
-
         if(game)
             {
             if(changes["name"]) game.Name = changes["name"];
@@ -155,17 +178,15 @@ postGame(data:{name:string, description:string, date:string}): object
             if(changes["fecha"]) game.Date = changes["fecha"];
             return {
                 "status": "201",
-                "game":{
+                "data":{
                     "id" : `${game.Id}`,
                     "date" : `${game.Date}`,
                     "description" : `${game.Purpose}`
-                    }
-                
-                    
+                    }   
             };
         }
         else{
-            throw("id not found");
+            throw(new Error("id not found"));
         }
     }
 }
