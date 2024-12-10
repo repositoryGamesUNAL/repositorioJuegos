ca49f992055c07130fe90c55007b41ce3507b1e8
Andres <aguido@unal.edu.co>
2024-12-10 10:13:57 -0500

    feat- primera versión de botones
    Falta los iconos y ya estaría lista la molécula

--- games-repository-frontend/src/Components/molecules/steps/steps.tsx
+++ games-repository-frontend/src/Components/molecules/steps/steps.tsx
@@ -1,34 +1,32 @@
 import React from "react";
-import styles from "./steps.module.scss";
+import styles from "./steps.module.scss"; // Asegúrate de que la ruta sea correcta
 import { StepProps } from "./steps.type";
 import { StepItem } from "./steps.type";
 
-
-
-
-
 const Step: React.FC<StepProps> = ({ steps, direction }) => {
-  return (
-    <div className={`${styles.step} ${styles[direction]}`}>
-      {steps.map((step, index) => (
-        <div
-          key={index}
-          className={`${styles.stepItem} ${styles[step.status]}`}
-        >
-        <div className={styles.icons}>
-          <div className={`${styles.step} ${styles[direction]}`}>
-            {step.status === "finished" && <span>✔️</span>}
-            {step.status === "process" && <span>⏳</span>}
-            {step.status === "error" && <span>❌</span>}
-            {step.status === "waiting" && <span>⚪</span>}
+    return (
+      <div className={`${ styles.step + " "+ styles[`step--${direction}`]}`}>
+        {steps.map((step: StepItem, index: number) => (
+          <div key={index} style={{
+              display: 'flex',
+              flexDirection: direction === "horizontal" ? "row" : "column",
+              justifyContent: 'center',
+              alignItems: 'center',
+              position: 'relative', // Asegúrate de que el contenedor sea relativo
+            }}>
+            <div className= {`${styles[`content--${direction}`]}`}>
+              <div className={`${ styles.circle + " "+ styles[`circle--${step.status}`]}`}></div>
+              <p className={`${ styles.label + " "+ styles[`label--${step.status}`]}`}>{step.description}</p>
+            </div>
+            {index < steps.length - 1 && ( // Añadir la barra solo si no es el último paso
+              <div className= {`${styles[`bar--${direction}`]} ${step.status === "error" || step.status === "waiting" ? styles["bar--disabled"] : ""}
+`}></div>
+            )}
           </div>
-          </div>
-          <p className={styles.description}>{step.description}</p>
-          {index < steps.length - 1 && <div className={styles.separator}></div>}
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  };
+  
 
 export default Step;
