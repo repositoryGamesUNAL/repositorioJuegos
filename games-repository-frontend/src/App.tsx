fd7352dad27e503298a84c3153e483919f03840a
Andres <aguido@unal.edu.co>
2024-12-11 09:10:06 -0500

    refactor-

--- games-repository-frontend/src/App.tsx
+++ games-repository-frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css'
 
-import Button from './Components/pruebas/pruebasBoton'
-import Input from './Components/pruebas/pruebas'
-import Headlines from './Components/pruebas/pruebaHealine';
-import Toggle from "./Components/pruebas/pruebaTogle"
-import Step from "./Components/pruebas/pruebaStep";
+
 function App() {
  
 
   return (
     <>
  
-<Toggle/>
+
 
   
       
