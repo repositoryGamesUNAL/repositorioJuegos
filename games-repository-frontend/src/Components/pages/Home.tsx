5b6c096fa51396dedde7a8fb2e37fa19860384ba
juanseb253 <jzapataec@unal.edu.co>
2024-12-19 23:56:32 -0500

    adding post connetion from frontend to backend

--- games-repository-frontend/src/Components/pages/Home.tsx
+++ games-repository-frontend/src/Components/pages/Home.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
-import InputList from '../molecules/inputList/inputList';
+import React from "react";
+import InputList from "../molecules/inputList/inputList";
+import axios from "axios";
 
+type SendGame = {
+  name: string;
+  purpose: string;
+  thematic: Array<string>;
+  genre: string;
+  materials: Array<string>;
+  objectives: string;
+  time: string;
+};
 const Home: React.FC = () => {
-    return (
-		<div>
-			<h1>Welcome to the Home Page</h1>
-			<InputList
-				placeholder='Concepto Fundamental'
-			/>
-		</div>
-	);
+  const handleSendGame = ({
+    name,
+    purpose,
+    thematic,
+    genre,
+    materials,
+    objectives,
+    time,
+  }: SendGame) => {
+    axios.post("http://localhost:3000/games", {
+      name,
+      purpose,
+      thematic,
+      genre,
+      materials,
+      objectives,
+      time,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <InputList placeholder="Concepto Fundamental" />
+      <button
+        onClick={() =>
+          handleSendGame({
+            name: "juego",
+            purpose: "cualquiera",
+            thematic: [],
+            genre: "genero",
+            materials: [],
+            objectives: "si",
+            time: "1 min",
+          })
+        }
+      >
+        enviar
+      </button>
+    </div>
+  );
 };
 
 export default Home;
