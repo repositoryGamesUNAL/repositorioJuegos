548d053bde600e7cd3475d7fdf676ea0ef009b5f
juanseb253 <jzapataec@unal.edu.co>
2024-12-19 23:57:21 -0500

    backend cors

--- games-repository-api/src/app.ts
+++ games-repository-api/src/app.ts
@@ -1,15 +1,18 @@
-import express from "express"
+import express from "express";
 import { Application } from "express";
 import initializeRoutes from "./routes";
-import { logError,errorResponse } from "./middlewares/errorMiddleware";
+import { logError, errorResponse } from "./middlewares/errorMiddleware";
+var cors = require("cors");
 
-
-const app:Application = express();
+const app: Application = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json()); //permite las solicitudes con body
 initializeRoutes(app);
-app.use(logError);//Error logging middleware
-app.use(errorResponse);//Error handling middleware
-
-
+app.use(logError); //Error logging middleware
+app.use(errorResponse); //Error handling middleware
 
 export default app;
